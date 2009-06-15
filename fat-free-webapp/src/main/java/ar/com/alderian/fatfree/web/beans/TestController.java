0225725449ccb1e1cb0ab601c1cd2f9893b415ac
Alderian <alderian@msn.com>
2009-06-14 23:16:21 -0300

    Add Test Service reference in TestController. Add value reference in test.jsp.

--- fat-free-webapp/src/main/java/ar/com/alderian/fatfree/web/beans/TestController.java
+++ fat-free-webapp/src/main/java/ar/com/alderian/fatfree/web/beans/TestController.java
@@ -3,18 +3,47 @@
  */
 package ar.com.alderian.fatfree.web.beans;
 
+import ar.com.alderian.fatfree.service.TestService;
+
 /**
  * @author oalvarez
  * 
  */
-public class TestCtrl {
+public class TestController {
+
+	private TestService testService;
 
 	/**
 	 * @return
 	 */
 	public String getLabel() {
 
-		return "Esta es una etiqueta";
+		return "Esta es una etiqueta.";
 
 	}
+
+	/**
+	 * @return
+	 */
+	public String getValue() {
+
+		return "Esta es el valor: " + testService.getValue();
+
+	}
+
+	/**
+	 * @return the testService
+	 */
+	public TestService getTestService() {
+		return testService;
+	}
+
+	/**
+	 * @param testService
+	 *            the testService to set
+	 */
+	public void setTestService(TestService testService) {
+		this.testService = testService;
+	}
+
 }
