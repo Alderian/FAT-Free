d3fc2928ccf8dedb41f58d474c78b48d9ccc86da
Alderian <alderian@msn.com>
2009-06-23 08:49:54 -0300

    Removed @Transactional from Services

--- fat-free-core/src/main/java/ar/com/alderian/fatfree/service/TestServiceImpl.java
+++ fat-free-core/src/main/java/ar/com/alderian/fatfree/service/TestServiceImpl.java
@@ -3,19 +3,19 @@
  */
 package ar.com.alderian.fatfree.service;
 
-import org.springframework.transaction.annotation.Transactional;
-
 import ar.com.alderian.fatfree.dao.TestDao;
 import ar.com.alderian.fatfree.entity.Test;
 
 /**
  * @author oalvarez
- *
+ * 
  */
-@Transactional
-public class TestServiceImpl extends AbstractBaseServiceImpl<Test> implements TestService {
-	
-	/* (non-Javadoc)
+public class TestServiceImpl extends AbstractBaseServiceImpl<Test> implements
+		TestService {
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see ar.com.alderian.fatfree.service.TestService#getValue()
 	 */
 	public String getValue() {
@@ -29,5 +29,5 @@ public class TestServiceImpl extends AbstractBaseServiceImpl<Test> implements Te
 	public void setTestDao(TestDao testDao) {
 		this.setBaseDao(testDao);
 	}
-	
+
 }
