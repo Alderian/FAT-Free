9b282dc56001b224670ae870f6a087c1d583ddbf
Alderian <alderian@msn.com>
2009-06-23 08:38:57 -0300

    Formatting sources

--- fat-free-core/src/main/java/ar/com/alderian/fatfree/dao/TestDao.java
+++ fat-free-core/src/main/java/ar/com/alderian/fatfree/dao/TestDao.java
@@ -7,9 +7,8 @@ import ar.com.alderian.fatfree.entity.Test;
 
 /**
  * @author oalvarez
- *
+ * 
  */
-public interface TestDao extends AbstractBaseDao<Test>{
-
+public interface TestDao extends AbstractBaseDao<Test> {
 
 }
